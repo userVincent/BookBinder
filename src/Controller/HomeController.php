ff89aaa895a2d7a3b172639f3888c6292b95506a
Vincent Vansant <vincent.vansant@student.kuleuven.be>
2023-05-08 00:40:49 +0200

    display most popular books on home page, just 10 first books for now

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,11 +12,14 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(BookRepository $bookRepository): Response
     {
-        $books = $bookRepository->findAll();
+        $books = $bookRepository->findBy([], null, 10);
         $user = $this->getUser();
+
+        //dd($books);
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'books' => $books,
+            'trending_books' => $books,
             'user' => $user,
         ]);
     }
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const Encore = require('@symfony/webpack-encore');
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/js/app.js')
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+;
+
+module.exports = Encore.getWebpackConfig();
