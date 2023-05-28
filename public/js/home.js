d6daf6735ca8cf000b272bcd10d030fc6266b04b
Hassin <hassinahmed.jalap@student.kuleuven.be>
2023-05-28 21:25:26 +0200

    feature to get trending books from NYT API working

--- public/js/home.js
+++ public/js/home.js
@@ -18,6 +18,125 @@ window.onclick = function(event) {
     }
 }
 
+window.onload = function() {
+    displayTrendingBooks();
+};
+
+
+function displayTrendingBooks() {
+    document.getElementById('output').innerHTML = "";
+
+    // Fetch the trending books from the NYT API
+    fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=NhXVWQ20PAjJUUAVw2n6OoH4KJW4U0X1")
+        .then(response => response.json())
+        .then(data => {
+            const books = data.results.books;
+
+            // Iterate through the books and display them
+            for (let i = 0; i < 12; i += 4) {
+                const book1 = books[i];
+                const book2 = books[i+1];
+                const book3 = books[i+2];
+                const book4 = books[i+3];
+
+                const title1 = book1.title;
+                const author1 = book1.author;
+                const isbn1 = book1.primary_isbn13;
+                const imageLinks1 = book1.book_image ? book1.book_image : "no-image-available.jpg"; // Default image if no thumbnail available
+
+                const title2 = book2.title;
+                const author2 = book2.author;
+                const isbn2 = book2.primary_isbn13;
+                const imageLinks2 = book2.book_image ? book2.book_image : "no-image-available.jpg"; // Default image if no thumbnail available
+
+                const title3 = book3.title;
+                const author3 = book3.author;
+                const isbn3 = book3.primary_isbn13;
+                const imageLinks3 = book3.book_image ? book3.book_image : "no-image-available.jpg"; // Default image if no thumbnail available
+
+                const title4 = book4.title;
+                const author4 = book4.author;
+                const isbn4 = book4.primary_isbn13;
+                const imageLinks4 = book4.book_image ? book4.book_image : "no-image-available.jpg"; // Default image if no thumbnail available
+
+                // Create the HTML
+                var booksHTML = `
+                    <div class="row">
+                    <div class="grid-container">
+                        <section class="book_container">
+                            <div class="book" style="display: flex; width: 50%; margin-bottom: 20px;">
+                            <a href="/book_profile/${encodeURIComponent(title1)}/${isbn1}">
+                                <div class="book-image" style="flex: 0 0 30%;">
+                                    <img src="${imageLinks1}" alt="Book Cover" style="width: 100%; height: 100%; object-fit: contain;">
+                                </div>
+                            </a>
+                            <div class="book-info" style="flex: 1;">
+                                <a href="/book_profile/${encodeURIComponent(title1)}/${isbn1}">
+                                <h3>${title1}</h3>
+                                </a>
+                                <p>${author1}</p>
+                            </div>
+                        </div>
+                        </section>
+                        
+                        <section class="book_container">
+                            <div class="book" style="display: flex; width: 50%; margin-bottom: 20px;">  
+                                <a href="/book_profile/${encodeURIComponent(title2)}/${isbn2}">
+                                <div class="book-image" style="flex: 0 0 30%;">
+                                    <img src="${imageLinks2}" alt="Book Cover" style="width: 100%; height: 100%; object-fit: contain;">
+                                </div>
+                                </a>
+                                <div class="book-info" style="flex: 1;">
+                                    <a href="/book_profile/${encodeURIComponent(title2)}/${isbn2}">
+                                    <h3>${title2}</h3>
+                                    </a>
+                                    <p>${author2}</p>
+                                </div>
+                            </div>
+                        </section>
+                                
+                        <section class="book_container">
+                            <div class="book" style="display: flex; width: 50%; margin-bottom: 20px;">
+                                <a href="/book_profile/${encodeURIComponent(title3)}/${isbn3}">
+                                    <div class="book-image" style="flex: 0 0 30%;">
+                                        <img src="${imageLinks3}" alt="Book Cover" style="width: 100%; height: 100%; object-fit: contain;">
+                                    </div>
+                                </a>
+                                <div class="book-info" style="flex: 1;">
+                                    <a href="/book_profile/${encodeURIComponent(title3)}/${isbn3}">
+                                        <h3>${title3}</h3>
+                                    </a>
+                                    <p>${author3}</p>
+                                </div>
+                            </div>
+                        </section>
+                        
+                        <section class="book_container">
+                            <div class="book" style="display: flex; width: 50%; margin-bottom: 20px;">
+                                <a href="/book_profile/${encodeURIComponent(title4)}/${isbn4}">
+                                    <div class="book-image" style="flex: 0 0 30%;">
+                                        <img src="${imageLinks4}" alt="Book Cover" style="width: 100%; height: 100%; object-fit: contain;">
+                                    </div>
+                                </a>
+                                <div class="book-info" style="flex: 1;">
+                                    <a href="/book_profile/${encodeURIComponent(title4)}/${isbn4}">
+                                        <h3>${title4}</h3>
+                                    </a>
+                                    <p>${author4}</p>
+                                </div>
+                            </div>
+                        </section>
+                        
+                    </div>
+                    </div>
+                `;
+
+                document.getElementById("output").innerHTML += booksHTML;
+            }
+        });
+}
+
+
 function getBooks() {
     document.getElementById('output').innerHTML = "";
     fetch("https://www.googleapis.com/books/v1/volumes?q=" + document.getElementById('input').value + "&maxResults=40")
